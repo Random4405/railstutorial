76a57dcf4399c6605ab78558da341e45d24252ff
Random4405 <random4405@gmail.com>
2017-08-22 01:12:35 +0300

    Finished 3rd chapter with rspec

--- Gemfile
+++ Gemfile
@@ -19,16 +19,17 @@ group :development do
   gem 'listen',                '3.0.8'
   gem 'spring',                '2.0.2'
   gem 'spring-watcher-listen', '2.0.1'
-  gem 'rspec-rails'
-  gem 'spring-commands-rspec'
-  gem 'guard-rspec', require: false
 end
 
 group :test do
+  gem 'rspec-rails'
+  gem 'spring-commands-rspec'
+  gem 'guard-rspec', require: false
+  gem 'rspec-html-matchers'
   gem 'rails-controller-testing', '1.0.2'
   gem 'minitest-reporters',       '1.1.14'
-  gem 'guard',                    '2.13.0'
-  gem 'guard-minitest',           '2.4.4'
+  # gem 'guard',                    '2.13.0'
+  # gem 'guard-minitest',           '2.4.4'
 end
 
 group :production do
--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,19 +1,69 @@
 require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
-  it "should get home" do
-    get :home
-    expect(response).to have_http_status(200)
+  render_views
+
+  before do
+    @base_title = "Ruby on Rails Tutorial Sample App"
   end
 
-  it "should get help" do
-    get :help
-    expect(response).to have_http_status(200)
+  describe "#home" do
+    before do
+      get :home
+    end
+
+    it "should get response 200" do
+      expect(response).to have_http_status(200)
+    end
+
+    it "should has right title" do
+      expect(response.body).to have_tag('title',
+        :text => "Home | #{@base_title}")
+    end
   end
 
-  it "should get about" do
-    get :about
-    expect(response).to have_http_status(200)
+  describe "#help" do
+    before do
+      get :help
+    end
+
+    it "should get response 200" do
+      expect(response).to have_http_status(200)
+    end
+
+    it "should has right title" do
+      expect(response.body).to have_tag('title',
+        :text => "Help | #{@base_title}")
+    end
   end
 
+  describe "#about" do
+    before do
+      get :about
+    end
+
+    it "should get response 200" do
+      expect(response).to have_http_status(200)
+    end
+
+    it "should has right title" do
+      expect(response.body).to have_tag('title',
+        :text => "About | #{@base_title}")
+    end
+  end
+
+  describe "#contact" do
+    before do
+      get :contact
+    end
+
+    it "should get response 200" do
+      expect(response).to have_http_status(200)
+    end
+
+    it "should has right title" do
+      expect(response.body).to have_tag('title',
+        :text => "Contact | #{@base_title}")
+    end
+  end
 end
