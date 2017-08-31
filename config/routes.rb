1113a9230798a7e2fcf8ed6da347ced13de893f2
Random4405 <random4405@gmail.com>
2017-08-31 20:12:53 +0300

    Finished activation account

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
   resources :users
+  resources :account_activations, only: [:edit]
   get '/signup', to: "users#new"
   post '/signup',  to: 'users#create'
   get '/help', to: "static_pages#help"
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,9 @@ User.create!(name:  "Nikita",
              email: "lazynick7@gmail.com",
              password:              "qwerty4405",
              password_confirmation: "qwerty4405",
-             admin: true)
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
 
 99.times do |n|
   name  = Faker::Name.name
@@ -19,5 +21,7 @@ User.create!(name:  "Nikita",
   User.create!(name:  name,
                email: email,
                password:              password,
-               password_confirmation: password)
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now)
 end
