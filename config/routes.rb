24539b5f7c5c1400e790359d414d399106814379
Random4405 <random4405@gmail.com>
2017-09-02 23:24:47 +0300

    Finished all chapters

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   get 'sessions/new'
   root 'static_pages#home'
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
   get '/signup', to: "users#new"
   post '/signup',  to: 'users#create'
   get '/help', to: "static_pages#help"
